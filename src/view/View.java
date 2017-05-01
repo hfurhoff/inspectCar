e25a882fae3e75661f76a01f48c5b472fb21cf1f
hampus <Coyote@DESKTOP-17273MG>
2017-05-01 21:40:54 +0200

    Added classes view and startup.

--- src/view/View.java
+++ src/view/View.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package inspectcar;
+package view;
 
 /**
- * 
+ * This class is a placeholder for the entire view.
  * @author Coyote
  */
-public class InspectCar {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class View {
     
 }
